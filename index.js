e6c5af6bf34fffcf3d050877e97b6caa4d834073
Timo <timo@teatwig.net>
2017-11-07 17:04:51 +0100

    Add volume control (although querying the current volume doesn't work consistently, probably because of the successive telnet connections?)

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ module.exports = function(homebridge) {
 function VSX(log, config) {
   this.log = log;
   this.name = config.name;
+  this.speakerName = config.speaker_name;
   this.HOST = config.ip;
   this.PORT = config.port;
 
@@ -30,15 +31,15 @@ VSX.prototype.getServices = function() {
     .on("set", this.setOn.bind(this))
     .on("get", this.getOn.bind(this));
 
-  this.speakerService = new Service.Speaker("lautsprecher");
+  this.speakerService = new Service.Speaker(this.speakerName);
   availableServices.push(this.speakerService);
 
   this.speakerService.getCharacteristic(Characteristic.Mute)
     .on("set", this.setMuted.bind(this))
     .on("get", this.getMuted.bind(this));
-  /*this.speakerService.addCharacteristic(new Characteristic.Volume())
+  this.speakerService.addCharacteristic(new Characteristic.Volume())
     .on("set", this.setVolume.bind(this))
-    .on("get", this.getVolume.bind(this));*/
+    .on("get", this.getVolume.bind(this));
 
   this.infoService = new Service.AccessoryInformation();
   availableServices.push(this.infoService);
@@ -191,11 +192,102 @@ VSX.prototype.getMuted = function(callback) {
 
 VSX.prototype.setMuted = function(muted, callback) {
 
+  if (muted) {
+    var client = new net.Socket();
+    client.connect(this.PORT, this.HOST, function() {
+      console.log('CONNECTED TO: ' + this.HOST + ':' + this.PORT);
+      // Write a message to the socket as soon as the client is connected, the server will receive it as message from the client
+      client.write('MO\r\n'); // toggle mute state
+      client.destroy();
+    });
+
+    //Add a 'close' event handler for the client sock
+    client.on('close', function() {
+      console.log('Connection closed');
+    });
+
+    client.on('error', function(ex) {
+      console.log("handled error");
+      console.log(ex);
+    });
+  } else {
+    var client = new net.Socket();
+    client.connect(this.PORT, this.HOST, function() {
+      console.log('CONNECTED TO: ' + this.HOST + ':' + this.PORT);
+      // Write a message to the socket as soon as the client is connected, the server will receive it as message from the client
+      client.write('MF\r\n'); // toggle mute state
+      client.destroy();
+    });
+
+    //Add a 'close' event handler for the client sock
+    client.on('close', function() {
+      console.log('Connection closed');
+    });
+
+    client.on('error', function(ex) {
+      console.log("handled error");
+      console.log(ex);
+    });
+  }
+  callback();
+
+}
+
+VSX.prototype.getVolume = function(callback) {
+
+  var client = new net.Socket();
+  client.connect(this.PORT, this.HOST, function() {
+
+    console.log('CONNECTED TO: ' + this.HOST + ':' + this.PORT);
+    client.write('?V\r\n');
+
+  });
+
+  client.on('data', function(data) {
+
+    console.log('DATA: ' + data);
+    var str = data.toString();
+
+    if (str.includes("VOL")) {
+      var volume = str.substring(3,6);
+      console.log("VOLUME: " + volume);
+      client.destroy();
+      callback(null,volume);
+
+    } else {
+      console.log("waiting");
+    }
+
+  });
+
+  client.on('close', function() {
+    console.log('Connection closed');
+  });
+
+  client.on('error', function(ex) {
+    console.log("handled error");
+    console.log(ex);
+    callback(ex);
+  });
+
+}
+
+VSX.prototype.setVolume = function(volume, callback) {
+
+  // VOL000 = -80.5 dB, VOL100 = -30.5 dB, VOL185 = +12 dB
+
+  // volume has to be padded with 0
+  if (volume < 10) {
+    volume = '00' + volume;
+  } else if (volume < 100) {
+    volume = '0' + volume;
+  }
+
   var client = new net.Socket();
   client.connect(this.PORT, this.HOST, function() {
     console.log('CONNECTED TO: ' + this.HOST + ':' + this.PORT);
     // Write a message to the socket as soon as the client is connected, the server will receive it as message from the client
-    client.write('MZ\r\n'); // toggle mute state
+    client.write(volume + 'VL\r\n'); // 050VL
     client.destroy();
   });
 
